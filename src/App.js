75dab8fa66de5eaa9ad20d75770702776f455ad5
AhmadSayed <ahmed.sayed.fcis1997@gmail.com>
2019-11-18 12:46:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as THREE from "three";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const draco3d = require("draco3d");
+const decoderModule = draco3d.createDecoderModule({});
+const encoderModule = draco3d.createEncoderModule({});
+
+class App extends Component {
+	state = {
+		selectedFile: null
+	};
+	fileSelectedHandler = event => {
+		console.log(event.target.files[0]);
+		this.setState({
+			selectedFile: event.target.files[0]
+		});
+	};
+
+	fileCompressHandler = () => {
+		
+	}
+	fileUploadHandler = () => {
+		const fd = new FormData();
+		fd.append(
+			"ObjectModel",
+			this.state.selectedFile,
+			this.state.selectedFile.name
+		);
+		axios
+			.post("API", fd, {
+				onUploadProgress: ProgressEvent => {
+					console.log(
+						"Upload Prograss: ",
+						Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + "%"
+					);
+				}
+			})
+			.then(res => {
+				console.log(res);
+			});
+	};
+	render() {
+		return (
+			<div>
+				<input type="file" onChange={this.fileSelectedHandler} />
+				<button onClick={this.fileUploadHandler}>Upload</button>
+				<div ref={ref => (this.mount = ref)} />
+			</div>
+		);
+	}
 }
 
 export default App;
